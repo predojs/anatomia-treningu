7e6369810dd62d7225051699bff3f5116044e872
Dawid <predecki.dawid@gmail.com>
2024-12-03 20:07:41 +0100

    Hamburger Menu

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,17 +1,46 @@
 import React from "react";
+import { useState } from "react";
 
-const Header = () => (
-  <header className="flex justify-between px-10 py-5 items-center sticky top-0">
-    <h1 className="text-4xl font-bold">Anatomia treningu</h1>
-    <nav>
-      <ul className="flex gap-x-8">
-        <li className="text-sm font-medium">Home</li>
-        <li className="text-sm font-medium">Treningi</li>
-        <li className="text-sm font-medium">O nas</li>
-        <li className="text-sm font-medium">Kontakt</li>
-      </ul>
-    </nav>
-  </header>
-);
+const Header = () => {
+  const [isMenuOpen, setIsMenuOpen] = useState(false);
+
+  return (
+    <header
+      className={
+        isMenuOpen
+          ? "flex-col sticky top-0 bg-white"
+          : "flex sticky top-0 bg-white"
+      }
+    >
+      <section className="flex w-full justify-between items-center px-6 py-5">
+        <h1 className="text-2xl font-bold">Anatomia treningu</h1>
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          fill="none"
+          viewBox="0 0 24 24"
+          strokeWidth={1.5}
+          stroke="currentColor"
+          className="size-5 md:hidden hover:cursor-pointer"
+          onClick={() => setIsMenuOpen((prev) => !prev)}
+        >
+          <path
+            strokeLinecap="round"
+            strokeLinejoin="round"
+            d="M3.75 6.75h16.5M3.75 12h16.5m-16.5 5.25h16.5"
+          />
+        </svg>
+      </section>
+
+      <nav>
+        <ul className="flex flex-col text-xl items-center pb-5 gap-5 absolute w-full bg-white">
+          <li className="font-medium hover:cursor-pointer">Home</li>
+          <li className="font-medium hover:cursor-pointer">Treningi</li>
+          <li className="font-medium hover:cursor-pointer">O nas</li>
+          <li className="font-medium hover:cursor-pointer">Kontakt</li>
+        </ul>
+      </nav>
+    </header>
+  );
+};
 
 export default Header;
--- src/views/IntroductionView/IntroductionView.js
+++ src/views/IntroductionView/IntroductionView.js
@@ -1,7 +1,7 @@
 import React from "react";
 
 const IntroductionView = () => (
-  <div className="flex flex-col items-center justify-center">
+  <div className="h-[80dvh] flex flex-col items-center justify-center">
     <h1 className="text-6xl font-bold text-center">Anatomia treningu</h1>
     <p className="text-xl text-center">
       Cześć! Witamy w naszym projekcie anatomii treningu. Zapraszamy do
